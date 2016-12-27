4ecfea62971f6b9eaf4934829006f0228a81586c
lucifer <181977814@qq.com>
2016-12-28 00:29:54 +0800

    update for entrust model doc.

--- app/models/Role.php
+++ app/models/Role.php
@@ -10,6 +10,18 @@ use Zizaco\Entrust\EntrustRole;
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\User[] $users
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Permission[] $perms
  * @mixin \Eloquent
+ * @property int $id
+ * @property string $name
+ * @property string $display_name
+ * @property string $description
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Query\Builder|\App\models\Role whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\models\Role whereName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\models\Role whereDisplayName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\models\Role whereDescription($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\models\Role whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\models\Role whereUpdatedAt($value)
  */
 class Role extends EntrustRole
 {
