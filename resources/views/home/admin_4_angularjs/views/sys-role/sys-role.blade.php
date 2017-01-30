8feded8b4e60b6bbf8f8d1f5fd7306bd56336175
lucifer <181977814@qq.com>
2017-01-30 23:48:02 +0800

    add data lang. update user controller. add role controller. update role model.

--- resources/views/home/admin_4_angularjs/views/sys-role/sys-role.blade.php
+++ resources/views/home/admin_4_angularjs/views/sys-role/sys-role.blade.php
@@ -10,7 +10,7 @@
         <div class="portlet-body">
             <div class="table-toolbar">
                 <button type="button" id="addData" class="btn btn-success" ng-click="addData()">增加</button>
-                <button type="button" id="delData" class="btn btn-danger" confirmation-needed="确定要删除这些用户吗？" ng-click="delData()">删除</button>
+                <button type="button" id="delData" class="btn btn-danger" confirmation-needed="确定要删除这些数据吗？" ng-click="delData()">删除</button>
                 <button type="button"  class="btn btn-info" ng-click="editData()">保存</button>
                 <button id='toggleFiltering' ng-click="toggleFiltering()" class="btn yellow">筛选</button>
                 <button id="refreshButton" type="button" class="btn purple-plum" ng-click="refreshData()">重获数据</button>
