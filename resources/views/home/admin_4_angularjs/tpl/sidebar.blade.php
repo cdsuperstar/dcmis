db39ac37137d7cba7eb79db484d950e2c150468c
homer <10835142@qq.com>
2017-01-30 17:53:54 +0800

    modify siderbar title  homer

--- resources/views/home/admin_4_angularjs/tpl/sidebar.blade.php
+++ resources/views/home/admin_4_angularjs/tpl/sidebar.blade.php
@@ -1,8 +1,8 @@
-<div class="page-sidebar navbar-collapse collapse">
+<div class="page-sidebar navbar-collapse collapse" style="margin-top: 10px;">
     <script type="text/ng-template" id="categoryTree">
         <a href="#@{{ ::tree.dcmodel.url }}" ng-if="::tree.children.length==0">
             <i class="@{{ ::tree.dcmodel.icon }}"></i>
-            @{{ ::tree.dcmodel.title }}
+            <span class="title">@{{ ::tree.dcmodel.title }}</span>
         </a>
         <a href="javascript:;" ng-if="::tree.children.length>0">
             <i class="@{{ ::tree.dcmodel.icon }}"></i>
@@ -16,6 +16,9 @@
     </script>
         <ul ng-repeat="tree in ::mdTreeJson " class="page-sidebar-menu" data-keep-expanded="false" data-auto-scroll="true"
             data-slide-speed="200" ng-class="{'page-sidebar-menu-closed': settings.layout.pageSidebarClosed}">
+            {{--<li class="heading">--}}
+                {{--<h3>菜单栏</h3>--}}
+            {{--</li>--}}
             <li class="sidebar-search-wrapper">
             </li>
             <li ng-repeat="tree in ::tree.children" ng-include="'categoryTree'"></li>
