d829ab329e6a832e09ec4baafb6fb009ba0936cf
lucifer <181977814@qq.com>
2017-01-30 16:34:39 +0800

    fix bootstrap conflict.

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -8,7 +8,7 @@ window._ = require('lodash');
  */
 
 //window.$ = window.jQuery = require('jquery');
-require('bootstrap-sass');
+//require('bootstrap-sass');
 //require('angular');
 require('angular-ui-grid');
 require('restangular');
