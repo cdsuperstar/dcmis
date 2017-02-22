009b3fcfad4e4a09419cde227c6cb101d03a456c
homer <10835142@qq.com>
2017-02-22 19:42:49 +0800

    modify printpage  homer

--- public/home/admin_4_angularjs/views/am-budget-list/am-budget-list.js
+++ public/home/admin_4_angularjs/views/am-budget-list/am-budget-list.js
@@ -305,6 +305,24 @@ angular.module("MetronicApp").controller('ambudgetlistCtrl',
 
             }
 
+            //执行打印操作
+            $scope.printsource = function () {
+                //$scope.printdate=$scope.gridApi.getRow();
+                var currentSelection = $scope.gridApi.selection.getSelectedRows();
+                if(currentSelection.length==1){
+                    //$scope.printdata = currentSelection[0];
+                    console.log(currentSelection[0].budgetname);
+                    window.open('views/am-budget-list/am-budgetprint.html');
+                    //var popupWindow = window.open('views/am-budget-list/am-budgetprint.html');
+                    //popupWindow.PrintSharedData = currentSelection[0];
+                    $scope.PrintSharedData = currentSelection[0];
+
+                }else{
+                    showMsg('选取行数错误。打印仅支持一条记录！<br>(当前状态：未选择或者选择大于2)', '错误', 'ruby');
+                }
+                //console.log($scope.printdate);
+
+            }
 
 
             $scope.toggleFiltering = function(){
