8860735cd10137f284e42626e615dcd31a6d4db8
cdsuperstar <181977814@qq.com>
2017-07-22 17:49:44 +0800

    add lb.no homer

--- resources/views/home/admin_4_angularjs/views/am-budget-lb/am-budget-lb.blade.php
+++ resources/views/home/admin_4_angularjs/views/am-budget-lb/am-budget-lb.blade.php
@@ -23,7 +23,7 @@
                             <div class="col-md-8">
                                 <div class="input-icon right">
                                     <i class="fa fa-warning tooltips font-red" data-original-title="必填项" data-container="body"></i>
-                                    <input type="text" class="form-control" ng-model="dcEdition.mno" placeholder="lb00">
+                                    <input type="text" class="form-control" ng-model="dcEdition.no" placeholder="lb00">
                                 </div>
                             </div>
                         </div>
@@ -32,7 +32,7 @@
                             <div class="col-md-8">
                                 <div class="input-icon right">
                                     <i class="fa fa-warning tooltips font-red" data-original-title="必填项" data-container="body"></i>
-                                    <input type="text" class="form-control" ng-model="dcEdition.name" placeholder="名称">
+                                    <input type="text" class="form-control" ng-model="dcEdition.type" placeholder="名称">
                                 </div>
                             </div>
                         </div>
@@ -41,7 +41,7 @@
                             <div class="col-md-8">
                                 <div class="input-icon right">
                                     <i class="fa fa-warning tooltips font-red" data-original-title="必填项" data-container="body"></i>
-                                    <input type="text" class="form-control" ng-model="dcEdition.mspell" placeholder="简拼">
+                                    <input type="text" class="form-control" ng-model="dcEdition.spell" placeholder="简拼">
                                 </div>
                             </div>
                         </div>
