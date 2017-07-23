7b4f8a5f6fb55a91fb787c83be4c389dbd9dd4df
cdsuperstar <181977814@qq.com>
2017-07-23 17:53:32 +0800

    update amsuppliers model

--- app/models/amsupplier.php
+++ app/models/amsupplier.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class amsupplier extends Model
 {
     //
+    protected $table = 'amsuppliers';
+    protected $fillable = ['compname', 'principal','tel', 'phone','compaddr', 'remark'];
+
 }
