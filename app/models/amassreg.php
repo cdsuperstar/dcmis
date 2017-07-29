01067c977cf01aa0d95de2a9ce4cd5f5b9773294
lucifer <181977814@qq.com>
2017-07-29 20:29:38 +0800

    update model

--- app/models/amassreg.php
+++ app/models/amassreg.php
@@ -40,7 +40,7 @@ class amassreg extends Model
     //
     protected $table = 'amassregs';
     protected $fillable = ['amasbudget_id','amt','asuser','unitgrps_id','userdate','validdate','state','remark'];
-    public function amasbudget(){
+    public function amasbudgets(){
         return $this->belongsTo('App\models\amasbudget');
     }
 
