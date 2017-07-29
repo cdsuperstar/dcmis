5e8091bc8117e5464c3e356244c4733139ca738d
cdsuperstar <181977814@qq.com>
2017-07-29 23:10:15 +0800

    modify assets-mangement-scrap homer

--- resources/views/home/admin_4_angularjs/views/am-assets-management-scrap/am-assets-management-scrap.blade.php
+++ resources/views/home/admin_4_angularjs/views/am-assets-management-scrap/am-assets-management-scrap.blade.php
@@ -2,18 +2,18 @@
 <div class="portlet-body" data-ng-controller="amassetmangementscrapCtrl">
     <div class="table-toolbar">
         <button id="saveData" type="button"  class="btn btn-primary" ng-click="editData()"><i class="fa fa-check"></i> 保存数据 </button>
-        <div class="btn-group btn-group-solid">
-            <button type="button" id="icon-basketset" class="btn btn-success dropdown-toggle" data-toggle="dropdown"><i class="fa fa-sign-out"></i> 物资状态处理  <i class="fa fa-angle-down"></i>
-            </button>
-            <ul class="dropdown-menu" >
-                <li>
-                    <a href="javascript:;" confirmation-needed="确定将这些物资状态置为【正常】吗？" ng-click="changeStatus('purchway','正常')"> 物资状态【正常】 </a>
-                </li>
-                <li>
-                    <a href="javascript:;" confirmation-needed="确定要报废这些资产吗？"  ng-click="changeStatus('purchway','报废')"> 物资状态【报废】 </a>
-                </li>
-            </ul>
-        </div>
+        {{--<div class="btn-group btn-group-solid">--}}
+            {{--<button type="button" id="icon-basketset" class="btn btn-success dropdown-toggle" data-toggle="dropdown"><i class="fa fa-sign-out"></i> 物资状态处理  <i class="fa fa-angle-down"></i>--}}
+            {{--</button>--}}
+            {{--<ul class="dropdown-menu" >--}}
+                {{--<li>--}}
+                    {{--<a href="javascript:;" confirmation-needed="确定将这些物资状态置为【正常】吗？" ng-click="changeStatus('purchway','正常')"> 物资状态【正常】 </a>--}}
+                {{--</li>--}}
+                {{--<li>--}}
+                    {{--<a href="javascript:;" confirmation-needed="确定要报废这些资产吗？"  ng-click="changeStatus('purchway','报废')"> 物资状态【报废】 </a>--}}
+                {{--</li>--}}
+            {{--</ul>--}}
+        {{--</div>--}}
         <button id='toggleFiltering' type="button"class="btn btn-warning" ng-click="toggleFiltering()" ><i class="fa fa-search"></i> 筛选数据 </button>
         <button id="refreshButton" type="button"  class="btn purple-plum" ng-click="refreshData()"><i class="fa fa-refresh"></i> 重获数据 </button>
 
