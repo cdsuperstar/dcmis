1e93c7ce98b2cec757835b6d2051679b7a03742c
homer <10835142@qq.com>
2017-08-03 00:35:32 +0800

    modify sys-password homer

--- public/home/admin_4_angularjs/views/sys-password/sys-password.js
+++ public/home/admin_4_angularjs/views/sys-password/sys-password.js
@@ -5,22 +5,25 @@ angular.module("MetronicApp").controller('syspasswordCtrl',
         function ($scope, Restangular, $q, $filter, ngDialog,uiGridConstants,i18nService) {
             i18nService.setCurrentLang('zh-cn');
 
-            if($scope.password.old && $scope.dcUser.id){
-                if($scope.password.new == $scope.password.repnew){
-                    var tableDatas = Restangular.all('/sys-users/setMyPassword');
-                    tableDatas.post(dcEdition).then(
-                        function (res) {
-                            if (res.success) {
-                                $scope.gridOptions.data.push(res);
-                                showMsg(res.messages.toString(), '信息', 'lime');
-                            } else {
-                                // TODO add error message to system
-                                showMsg(res.errors.toString(), '错误', 'ruby');
+            $scope.edituserpwd = function () {
+                if($scope.dcEditionPWD.oldpwd && $scope.dcUser.id && $scope.dcEditionPWD.newpwd){
+                    if($scope.dcEditionPWD.newpwd == $scope.dcEditionPWD.repnew){
+                        var tableDatas = Restangular.all('/sys-users/setMyPassword');
+                        var dcEditionP = {"oldpwd":$scope.dcEditionPWD.oldpwd,"newpwd":$scope.dcEditionPWD.newpwd};
+                        tableDatas.post(dcEditionP).then(
+                            function (res) {
+                                if (res.success) {
+                                    showMsg(res.messages.toString(), '信息', 'lime');
+                                } else {
+                                    // TODO add error message to system
+                                    showMsg(res.errors.toString(), '错误', 'ruby');
+                                }
                             }
-                        }
-                    );
-                }else showMsg('所设置密码两次输入不一致（校验错误），请重新输入！', '错误', 'ruby');
-            }
+                        );
+                    }else showMsg('所设置密码两次输入不一致（校验错误），请重新输入！', '错误', 'ruby');
+                }
+            };
+
         }
     ]
 )
--- resources/views/home/admin_4_angularjs/views/sys-password/sys-password.blade.php
+++ resources/views/home/admin_4_angularjs/views/sys-password/sys-password.blade.php
@@ -6,7 +6,7 @@
             <div class="col-md-4">
                 <div class="input-icon right">
                     <i class="fa fa-warning tooltips font-red" data-original-title="必填项" data-container="body"></i>
-                    <input type="password" class="form-control" ng-model="password.old" placeholder="旧密码">
+                    <input type="password" class="form-control" ng-model="dcEditionPWD.oldpwd" placeholder="旧密码">
                 </div>
             </div>
         </div>
@@ -15,7 +15,7 @@
             <div class="col-md-4">
                 <div class="input-icon right">
                     <i class="fa fa-warning tooltips font-red" data-original-title="必填项" data-container="body"></i>
-                    <input type="password" class="form-control" ng-model="password.new" placeholder="新密码">
+                    <input type="password" class="form-control" ng-model="dcEditionPWD.newpwd" placeholder="新密码">
                 </div>
             </div>
         </div>
@@ -24,13 +24,13 @@
             <div class="col-md-4">
                 <div class="input-icon right">
                     <i class="fa fa-warning tooltips font-red" data-original-title="必填项" data-container="body"></i>
-                    <input type="password" class="form-control" ng-model="password.repnew" placeholder="确认密码">
+                    <input type="password" class="form-control" ng-model="dcEditionPWD.repnew" placeholder="确认密码">
                 </div>
             </div>
         </div>
         <div class="form-group">
             <div class="col-md-offset-1 col-md-3 col-sm-6 col-xs-6">
-                <a href="javascript:;" class="btn green" ng-click="edituserprofile()">
+                <a href="javascript:;" class="btn green" ng-click="edituserpwd()">
                     <i class="fa fa-check"></i> 确认修改 </a>
             </div>
         </div>
