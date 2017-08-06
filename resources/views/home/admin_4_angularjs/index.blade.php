ea0b70f3e61e911986a9d528fb6a56eb3556b7c3
lucifer <181977814@qq.com>
2017-08-06 15:35:22 +0800

    fix 360 compatibility

--- resources/views/home/admin_4_angularjs/index.blade.php
+++ resources/views/home/admin_4_angularjs/index.blade.php
@@ -11,13 +11,14 @@
 
 <head>
     <meta name="renderer" content="webkit|ie-comp|ie-stand">
-    <title data-ng-bind="'资产管理系统 | ' + $state.current.data.pageTitle"></title>
     <meta charset="utf-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1" name="viewport"/>
     <meta content="" name="description"/>
     <meta content="" name="author"/>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title data-ng-bind="'资产管理系统 | ' + $state.current.data.pageTitle"></title>
     <script src="//{{ Request::getHost() }}:6001/socket.io/socket.io.js"></script>
 
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
