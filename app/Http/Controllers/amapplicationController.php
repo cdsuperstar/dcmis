48f0751d2b639ddae5214cd178c20255338b3cdf
vagrant <vagrant@homestead>
2019-05-04 14:07:07 +0000

    update as req

--- app/Http/Controllers/amapplicationController.php
+++ app/Http/Controllers/amapplicationController.php
@@ -142,7 +142,7 @@ class amapplicationController extends Controller
             foreach ($aSubs as $k => $v) {
                 $cntSub = 0;
                 $v['amapplication_id'] = $rec->id;
-                if(!isset($v["amt"]))$v["amt"]=1;
+                if(!isset($v["reqamt"]))$v["reqamt"]=1;
                 if (isset($v["id"])) {
                     $recSub[$k] = amsubbudget::find($v["id"]);
                     $recSub[$k]->fill($v);
